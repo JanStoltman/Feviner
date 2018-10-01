4643639e05ce080a7888c5ddaf400e9d1616e201
Jan <stoltmanjan@gmail.com>
2018-10-01 14:06:26 +0200

    Link update

--- app/src/main/java/com/yggdralisk/feviner/dagger/components/MainActivityComponent.kt
+++ app/src/main/java/com/yggdralisk/feviner/dagger/components/MainActivityComponent.kt
@@ -1,10 +1,10 @@
 package com.yggdralisk.feviner.dagger.components
 
 import com.yggdralisk.feviner.MainActivity
-import com.yggdralisk.feviner.dagger.modules.UserScoreModule
+import com.yggdralisk.feviner.dagger.modules.MainActivityModule
 import dagger.Component
 
-@Component(modules = [UserScoreModule::class])
-interface MainComponent {
+@Component(modules = [MainActivityModule::class])
+interface MainActivityComponent {
     fun inject(activity: MainActivity)
 }
